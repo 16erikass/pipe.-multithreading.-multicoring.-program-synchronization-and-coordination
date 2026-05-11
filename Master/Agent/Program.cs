f95ce3f44770e8e680a188771aab90f6fda4ca7a
16erikass <16erikass@gmail.com>
2026-05-11 11:35:56 +0300

    sukurti master ir agent

---
+++ Master/Agent/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.IO.Pipes;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        string name = args[0];
+
+        using NamedPipeClientStream pipe =
+            new (
+                ".",
+                "mypipe",
+                PipeDirection.InOut);
+
+        await pipe.ConnectAsync();
+
+        using StreamReader reader =
+            new (pipe);
+
+        using StreamWriter writer =
+            new (pipe);
+
+        writer.AutoFlush = true;
+
+        // gaunam skaiciu
+        string? line = await reader.ReadLineAsync();
+
+        if (line == null)
+        {
+            Console.WriteLine("No data received.");
+            return;
+        }
+
+        int secret = int.Parse(line);
+
+        Random rnd = new();
+
+        while (true)
+        {
+            int guess = rnd.Next(1, 101);
+
+            if (guess == secret)
+            {
+                string msg =
+                    $"Agentas {name} atspejo {secret}";
+
+                await writer.WriteLineAsync(msg);
+
+                break;
+            }
+        }
+    }
+}
---
+++ Master/Master/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Diagnostics;
+using System.IO.Pipes;
+
+class Program
+{
+    static async Task Main()
+    {
+        int secret = new Random().Next(1, 101);
+
+        Console.WriteLine($"Skaicius: {secret}");
+
+        Stopwatch sw = Stopwatch.StartNew();
+
+        // paleidziam agentus
+        Process.Start("Agent.exe", "1");
+        Process.Start("Agent.exe", "2");
+
+        // laukiam 2 agentu
+        for (int i = 0; i < 2; i++)
+        {
+            _ = HandleAgent(secret, sw);
+        }
+
+        Console.ReadLine();
+    }
+
+    static async Task HandleAgent(
+        int secret,
+        Stopwatch sw)
+    {
+        using NamedPipeServerStream pipe =
+            new (
+                "mypipe",
+                PipeDirection.InOut,
+                2,
+                PipeTransmissionMode.Byte,
+                PipeOptions.Asynchronous);
+
+        await pipe.WaitForConnectionAsync();
+
+        using StreamReader reader =
+            new (pipe);
+
+        using StreamWriter writer =
+            new (pipe);
+
+        writer.AutoFlush = true;
+
+        // issiunciam skaiciu
+        await writer.WriteLineAsync(secret.ToString());
+
+        // gaunam atsakyma
+        string? msg =
+            await reader.ReadLineAsync();
+
+        Console.WriteLine(msg);
+
+        Console.WriteLine(
+            $"Laikas: {sw.ElapsedMilliseconds} ms");
+    }
+}
